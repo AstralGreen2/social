b75b62da657cb8267e85908c10604370cc49ec26
VB <belov.vlad2002@yandex.ru>
2020-02-16 16:53:28 +0300

    16.02.2020

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,72 @@
 <template>
   <v-app id="app">
+
+
+
+    
     <v-navigation-drawer color="green" dark
     expand-on-hover 
     hide-overlay
     permanent
     right
-    app>
+    app
+    >
 
-        <v-list
-    nav 
-    shaped
-    dense
-      >
+      
 
 
-    <v-list-item>
+        <v-list nav  shaped  dense>
+
+
+    <v-list-item >
+      <v-list-item-avatar>
+          <img src=" https://randomuser.me/api/portraits/men/1.jpg" alt="">
+      </v-list-item-avatar>
+
+      <v-list-item-content class = "text-left">
+          <v-list-item-title class="font-weight-black">SocialLink</v-list-item-title>
+          <v-list-item-subtitle>Гена</v-list-item-subtitle>
+      </v-list-item-content>
       
     </v-list-item>
-    
+    <v-divider class="my-3"></v-divider>
+
+    <v-list-item>
+      <v-list-item-icon>
+        <v-icon>mdi-home-outline</v-icon>
+      </v-list-item-icon>
+
+      <v-list-item-content>
+        <v-list-item-title class="text-left">Главная</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+
+     <v-list-item link to="/users/id">
+      <v-list-item-icon>
+        <v-icon>mdi-home-outline</v-icon>
+      </v-list-item-icon>
+
+      <v-list-item-content>
+        <v-list-item-title class="text-left">Моя страница</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+
+
+     <v-list-item link to="/users">
+      <v-list-item-icon>
+        <v-icon>mdi-home-outline</v-icon>
+      </v-list-item-icon>
+
+      <v-list-item-content>
+        <v-list-item-title class="text-left">Найти друзей</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+   
     </v-list>
     </v-navigation-drawer>
+
     <v-content class="px-12 py-3">
       <v-container fluid>
         <router-view/>
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import User from '../views/User.vue'
+import Users from '../views/Users.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
+  
     {
       path: '/login',
       component:Login
     },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
+    {
+      path: '/users/:id',
+      component : User
+    },
+    {
+      path: '/users',
+      component : Users
+    }
 ]
 
 const router = new VueRouter({
